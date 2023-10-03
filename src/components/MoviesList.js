7fd01eb90ef248082ddcb04ca1c198fb47bbe807
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2023-10-04 00:08:45 +0300

    edited for smaller screens

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -11,7 +11,7 @@ const MovieList = ({
 }) => {
   return (
     <>
-      <div className="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-5 g-4">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-5 g-4">
         {movieList.map((movie) => (
           <div className="col" key={movie.id}>
             <MovieCard movieItem={movie} />
