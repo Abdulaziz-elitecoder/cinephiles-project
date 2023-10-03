a480b97dfe6f9ef40ac5f5742752f9b5c403c848
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2023-10-04 00:14:28 +0300

    adjusted some minor problems

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import theme from "./slices/theme.js";
+import theme from "./slices/Theme.js";
 import wishlistReducer from "./slices/wishlist.js";
 import language from "./slices/language.js";
 
